1b3d1aff6340d978d57031d98b9b672a57fd540f
Josef Bischof <josefmb17@gmail.com>
2023-12-06 10:15:38 -0300

    console log

--- client/src/redux/reducers/cartReducers.jsx
+++ client/src/redux/reducers/cartReducers.jsx
@@ -13,21 +13,21 @@ import {
     switch (action.type) {
       case CART_ADD_ITEM:
         const item = action.payload;
-        const existItem = state.cartItems.find((x) => x.product === item.product);
+        // const existItem = state.cartItems.find((x) => x.product === item.product);
   
-        if (existItem) {
-          return {
-            ...state,
-            cartItems: state.cartItems.map((x) =>
-              x.product === existItem.product ? item : x
-            ),
-          };
-        } else {
+        // if (existItem) {
+        //   return {
+        //     ...state,
+        //     cartItems: state.cartItems.map((x) =>
+        //       x.product === existItem.product ? item : x
+        //     ),
+        //   };
+        // } else {
           return {
             ...state,
             cartItems: [...state.cartItems, item],
           };
-        }
+        //}
       case CART_REMOVE_ITEM:
         return {
           ...state,
